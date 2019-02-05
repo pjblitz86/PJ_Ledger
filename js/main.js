8a86d3cb3b4a0af466056504bbff4ad6aefb12be
Paulius Juknis <paulius.juknis@gmail.com>
2019-02-05 15:14:23 +0200

    smooth scrolling with js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Google Maps API
 function initMap() {
   const loc = { lat: 54.898521, lng: 23.903597 };
   const map = new google.maps.Map(document.querySelector(".map"), {
@@ -7,3 +8,28 @@ function initMap() {
 
   const maker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Sticky menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 65
+      },
+      800
+    );
+  }
+});
